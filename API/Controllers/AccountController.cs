1bdd56b0d96c0194c6e83803efdda8b74e5b35bd
Abidstic <abid71@student.sust.edu>
2023-03-26 23:07:35 +0600

    Added DTOs folder to bind client and server side

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,44 +1,48 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController: BaseController
+    public class AccountController : BaseController
     {
         private readonly DataContext _context;
         public AccountController(DataContext context)
         {
             _context = context;
-            
+
         }
 
         [HttpPost("register")] //api/account/register
 
-        public async Task<ActionResult<AppUser>> Register (string username, string passowrd)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if (await UserExist(registerDto.Username)) return BadRequest("User Already exists");
+
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
-                UserName= username,
-                PasswordHash= hmac.ComputeHash(Encoding.UTF8.GetBytes(passowrd)),
-                PasswordSalt= hmac.Key
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
             };
 
             _context.Users.Add(user);
 
             await _context.SaveChangesAsync();
 
+
             return user;
         }
 
-
-        
-
-
-        
+        private async Task<bool> UserExist(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
     }
 }
