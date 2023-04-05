816df2f83429e25ff8aa5c96cebe8c7573d007d2
Abidstic <abid71@student.sust.edu>
2023-04-05 17:49:23 +0600

    made change in data type

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -11,7 +11,7 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model:any={};
  
-  currentUser: User | null=null;
+  currentUser:any;
   
   
 
